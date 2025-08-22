069c8aed770d71726b4134c04d721f150f559bf0
shahadat200 <shahadat.aust2@gmail.com>
2025-08-22 15:33:38 +0600

    backend api updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
-import { Routes,Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/home'
 import Navbar from './components/Navbar'
+import Register from './components/registration'
+import Login from './components/login'
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <Routes>
-        <Route path='/' element={<Home/>} >
-        </Route >
+        <Route path='/' element={<Home />} />
+       
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+       
       </Routes>
-      
     </div>
   )
 }
